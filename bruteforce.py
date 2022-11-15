92e54a080cc92c508ac66d855425a708d542c4be
Grolsch Security <grolsch@protonmail.com>
2022-11-15 10:51:56 +0100

    Adding docstrings

--- bruteforce.py
+++ bruteforce.py
@@ -13,6 +13,9 @@ class Bruteforce:
         self.best_investment = 0
 
     def generate_bruteforce_list(self):
+        """
+        Generate all the permutations from 1 to len(data).
+        """
         i = 1
         while i != len(self.data):
             perm = list(permutations(self.data.index, i))
@@ -21,10 +24,20 @@ class Bruteforce:
             i += 1
 
     def migrate_list_elements(self, res):
+        """
+        Migrate the sorted permutations to self.list.
+        Args:
+            res: the element to migrate.
+        """
         for elm in res:
             self.list.append(elm)
 
     def remove_duplicates(self, perm):
+        """
+        Sort the list of permutations and remove duplicate.
+        Args:
+            perm: the permutations list.
+        """
         new_list = []
         for elm in perm:
             new_list.append(list(elm))
@@ -35,24 +48,51 @@ class Bruteforce:
         self.migrate_list_elements(res)
 
     def get_money_invested(self, shares):
+        """
+        Calculate how much money costed a list of share.
+        Args:
+            shares: the list of share
+
+        Returns:
+            the cost of the list of share
+        """
         price = 0
         for elm in shares:
             price += int(self.data["Price"][elm])
         return price
 
     def remove_constraint_overflow(self):
+        """
+        Check if the cost is less than 500 and add the index of the list and the cost to a dictionary.
+        """
         for elm in self.list:
             total_price = self.get_money_invested(elm)
             if total_price < 500:
                 self.dico.update({self.list.index(elm): [total_price]})
 
     def calculate_single_profit(self, elm):
+        """
+        Calculate the profit for a single share.
+        Args:
+            elm: the index of the share.
+        Returns:
+            the profit for that share
+        """
         price = self.data["Price"].loc[elm]
         profit_percent = self.data["Profit"].loc[elm]
         profit = (price * ((profit_percent / 100) + 1)) - price
         return profit
 
     def calculate_total_profit(self, index):
+        """
+        Calculate the total profit for a list of share.
+        Args:
+            index: the index of the list of share.
+
+        Returns:
+            the total profit for the list of share.
+
+        """
         total_profit = 0
         for elm in self.list[index]:
             profit = self.calculate_single_profit(elm)
@@ -62,11 +102,20 @@ class Bruteforce:
         return total_profit
 
     def update_dico(self, index, profit):
+        """
+        Update the dictionary.
+        Args:
+            index: the index of the list of share and at the same time the key of the dictionary.
+            profit: The value to add to the dictionary.
+        """
         new_list = self.dico[index]
         new_list.append(profit)
         self.dico.update({index: new_list})
 
     def find_best_investment(self):
+        """
+        Find the investment that make the better profit.
+        """
         self.best_investment = None
         for key in self.dico.keys():
             profit = self.calculate_total_profit(key)
@@ -75,6 +124,9 @@ class Bruteforce:
                 self.best_investment = key
 
     def generate_report(self):
+        """
+        Generate a csv report with the details of the best investment to do.
+        """
         shares_index = self.list[self.best_investment]
         other_data = self.dico[self.best_investment]
         df = DataFrame(
@@ -103,6 +155,9 @@ class Bruteforce:
         df.to_csv("Best-Investment.csv")
 
     def force(self):
+        """
+        The main function that include all the step to bruteforce the best investment.
+        """
         self.generate_bruteforce_list()
         self.remove_constraint_overflow()
         self.find_best_investment()
