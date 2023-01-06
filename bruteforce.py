43a21387bc3a8163ca53669adc2f042c385354ac
Romain <romain@iMac-de-Romain.local>
2023-01-06 15:05:11 +0100

    Final Bruteforce solution

--- bruteforce.py
+++ bruteforce.py
@@ -1,174 +1,128 @@
-from itertools import permutations
-from sys import argv, exit
-from pandas import read_csv, DataFrame
+from pandas import DataFrame, read_csv
+from copy import copy
+from sys import argv, exit, setrecursionlimit
 
 
 class Bruteforce:
-    def __init__(self, file_csv):
-        self.wallet = 500
-        self.list = []
-        self.data = read_csv(file_csv, names=["Share", "Price", "Profit"], header=0)
-        self.dico = {}
-        self.best_profit = 0
-        self.best_investment = 0
+    def __init__(self, recursion, file_csv, wallet=500):
+        self.data = None
+        self.pre_data_treatment(file_csv)
+        self.wallet = wallet
+        self.best_solution = None
+        setrecursionlimit(recursion)
 
-    def generate_bruteforce_list(self):
+    def pre_data_treatment(self, file_csv):
         """
-        Generate all the permutations from 1 to len(data).
-        """
-        i = 1
-        while i != len(self.data) + 1:
-            perm = list(permutations(self.data.index, i))
-            self.remove_duplicates(perm)
-            print(f"Generated {len(self.list)} Combination !")
-            i += 1
-
-    def migrate_list_elements(self, res):
-        """
-        Migrate the sorted permutations to self.list.
+        Read a csv file and create a new dataframe containing same data as the csv,
+        only the profit column change from percent to value.
         Args:
-            res: the element to migrate.
+            file_csv: the csv file to read.
         """
-        for elm in res:
-            self.list.append(elm)
-
-    def remove_duplicates(self, perm):
-        """
-        Sort the list of permutations and remove duplicate.
-        Args:
-            perm: the permutations list.
-        """
-        new_list = []
-        for elm in perm:
-            new_list.append(list(elm))
-        for elm in new_list:
-            elm.sort()
-        res = []
-        [res.append(x) for x in new_list if x not in res]
-        self.migrate_list_elements(res)
-
-    def get_money_invested(self, shares):
-        """
-        Calculate how much money costed a list of share.
-        Args:
-            shares: the list of share
-
-        Returns:
-            the cost of the list of share
-        """
-        price = 0
-        for elm in shares:
-            price += int(self.data["Price"][elm])
-        return price
-
-    def remove_constraint_overflow(self):
-        """
-        Check if the cost is less than 500 and add the index of the list and the cost to a dictionary.
-        """
-        for elm in self.list:
-            total_price = self.get_money_invested(elm)
-            if total_price < self.wallet:
-                self.dico.update({self.list.index(elm): [total_price]})
-
-    def calculate_single_profit(self, elm):
-        """
-        Calculate the profit for a single share.
-        Args:
-            elm: the index of the share.
-        Returns:
-            the profit for that share
-        """
-        price = self.data["Price"].loc[elm]
-        profit_percent = self.data["Profit"].loc[elm]
-        profit = (price * ((profit_percent / 100) + 1)) - price
-        return profit
-
-    def calculate_total_profit(self, index):
-        """
-        Calculate the total profit for a list of share.
-        Args:
-            index: the index of the list of share.
-
-        Returns:
-            the total profit for the list of share.
-
-        """
-        total_profit = 0
-        for elm in self.list[index]:
-            profit = self.calculate_single_profit(elm)
-            total_profit += profit
-            self.update_dico(index, profit)
-        self.update_dico(index, total_profit)
-        return total_profit
-
-    def update_dico(self, index, profit):
-        """
-        Update the dictionary.
-        Args:
-            index: the index of the list of share and at the same time the key of the dictionary.
-            profit: The value to add to the dictionary.
-        """
-        new_list = self.dico[index]
-        new_list.append(profit)
-        self.dico.update({index: new_list})
-
-    def find_best_investment(self):
-        """
-        Find the investment that make the better profit.
-        """
-        self.best_investment = None
-        for key in self.dico.keys():
-            profit = self.calculate_total_profit(key)
-            if profit > self.best_profit:
-                self.best_profit = profit
-                self.best_investment = key
-
-    def generate_report(self):
-        """
-        Generate a csv report with the details of the best investment to do.
-        """
-        shares_index = self.list[self.best_investment]
-        other_data = self.dico[self.best_investment]
-        df = DataFrame(
-            columns=[
-                "Share",
-                "Price",
-                "Profit after 2 years (percent)",
-                "Profit after 2 years (values)",
-            ]
+        brute_datas = read_csv(
+            file_csv, names=["Share Name", "Price", "Profit"], header=0
         )
-        i = 1
-        for share in shares_index:
+        df = DataFrame(columns=["Share Name", "Price", "Profit"])
+        for i in range(0, len(brute_datas)):
             df.loc[len(df)] = [
-                self.data["Share"].iloc[share],
-                self.data["Price"].iloc[share],
-                self.data["Profit"].iloc[share],
-                round(other_data[i], 2),
+                brute_datas.iloc[i].loc["Share Name"],
+                brute_datas.iloc[i].loc["Price"],
+                round(
+                    brute_datas.iloc[i].loc["Price"]
+                    * float(brute_datas.iloc[i].loc["Profit"])
+                    / 100,
+                    2,
+                ),
             ]
-            i += 1
-        df.loc[len(df)] = [
-            "Total cost",
-            other_data[0],
-            "Total profit",
-            round(other_data[len(other_data) - 1], 2),
-        ]
-        df.to_csv("Best-Investment.csv", index=False)
+        self.data = df
+
+    def knapsack(self, solution, current_index, wallet):
+        """
+        The Recursive way of solving the knapsack problem.
+        Args:
+            solution: an array containing the indexes that respect the constraint.
+            current_index: the current index.
+            wallet: the money available on the wallet.
+        """
+        if current_index < 0 or wallet == 0:
+            return solution
+        # res1 = self.knapsack(solution, currentIndex - 1, wallet)
+        # res2 = None
+        if self.data.iloc[current_index].loc["Price"] > wallet:
+            return self.knapsack(solution, current_index - 1, wallet)
+        else:
+            res_1 = self.knapsack(solution, current_index - 1, wallet)
+            nl = copy(solution)
+            nl.append(current_index)
+            return self.find_best(
+                res_1,
+                self.knapsack(
+                    nl,
+                    current_index - 1,
+                    wallet - self.data.iloc[current_index].loc["Price"],
+                ),
+            )
+
+    # Si le current index est egale ou depasse la len(dataset)
+    # On retourne la solution
+    # On appelle a nouveau la fonction avec l'index i + 1 et on enregistre le resultat dans une variable
+    # Si le cost + le prix du share a currentIndex ne depasse pas la contrainte
+    # Alors on ajoute a solution le current index
+    # On appelle a nouveau la fonction avec le cost = cost + price de currentIndex,
+    # currentIndex + 1 on stocke le resultat
+    # return appelle a une fonction find best avec res1 res2
+
+    def find_best(self, res1, res2):
+        """
+        Compare the best profit between two solutions.
+        Args:
+            res1: the first solution
+            res2: the second solution
+
+        Returns:
+            the best solution based on the profit
+        """
+        if res2 is None:
+            return res1
+        profit1 = 0
+        profit2 = 0
+        for elm in res1:
+            profit1 += self.data.iloc[elm].loc["Profit"]
+        for elm in res2:
+            profit2 += self.data.iloc[elm].loc["Profit"]
+        res = max(profit1, profit2)
+        if res == profit1:
+            return res1
+        return res2
+
+    def print_solution(self):
+        """
+        Print the best solution found.
+        """
+        cost = 0
+        profit = 0
+        for elm in self.best_solution:
+            cost += self.data.iloc[elm].loc["Price"]
+            profit += self.data.iloc[elm].loc["Profit"]
+            print(
+                f"{self.data.iloc[elm].loc['Share Name']} | "
+                f" {self.data.iloc[elm].loc['Price']} | "
+                f"{self.data.iloc[elm].loc['Profit']}"
+            )
+        print(f"Total cost: {cost} | Total profit: {profit}")
 
     def force(self):
         """
-        The main function that include all the step to bruteforce the best investment.
+        The main execution of the program.
+        Returns:
+
         """
-        self.generate_bruteforce_list()
-        self.remove_constraint_overflow()
-        self.find_best_investment()
-        self.generate_report()
+        self.best_solution = self.knapsack([], len(self.data) - 1, self.wallet)
+        self.print_solution()
 
 
 if __name__ == "__main__":
     if len(argv) != 2:
-        print("Usage: ./bruteforce.py <file.csv>")
-        exit(1)
-    else:
-        brute = Bruteforce(argv[1])
-        brute.force()
+        print("Usage: python3 bruteforce2.py <csv_file>")
         exit(0)
+    brute = Bruteforce(2000, argv[1])
+    brute.force()
